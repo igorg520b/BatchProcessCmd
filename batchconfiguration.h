c028ae490983ef6597ce9299edd724d7f0ad28fb
Igor Gribanov <igor1215@gmail.com>
2022-08-27 16:29:52 -0230

    Update batchconfiguration.h

--- batchconfiguration.h
+++ batchconfiguration.h
@@ -15,27 +15,32 @@
 class BatchConfiguration
 {
 public:
-    BatchConfiguration() = default;
-
 
     QString mshFileName;
-    int dim = 3;
+    int dims = 2;
 
+    double indenterRadius = 0.161925;
+    double indentationDepth = 0.05;
+    double horizontalOffset = 0.2;
+    bool plasticity = true;
 
-    double czsStrength = 4e5;
-    double YoungsModulus = 9e9;
-    double czElasticity = 5e9, czEnergy = 50;
-    int numberOfCores; // for job
+    double timeToRun = 0.2;
+    int nFrames = 200;
 
-    double indenterRadius=0.05;
-    double indenterDepth=0;
-    double indentationRate=0.001;
-    double horizontalOffset=0;
+    bool loadWithIndenter = true;   // if false -> static load
+
+    constexpr static double blockHeight = 0.5;
+    constexpr static double blockLength = 0.6;
+    constexpr static double indentationRate = 0.2;
+
+    constexpr static int numberOfCores = 12;
+    constexpr static double YoungsModulus = 9e9;
+    constexpr static double czsStrength = 4e5;
+    constexpr static double czElasticity = 5e9;
+    constexpr static double czEnergy = 50;
+
     bool insertCZs = true;
 
-    enum ConfinementType { bottomOnly=0, full=1, sides=2, frontAndBack=3 };
-
-    ConfinementType confinement = ConfinementType::full;
 
     QString batchFileName;          // where this configuration is saved
     QString BatchName() const;      // same as batchFileName, without extension and path
@@ -49,6 +54,9 @@ public:
     void GeneratePythonScript();      // save .PY files into a fodler
 
 private:
+    void GeneratePythonScript2D();
+    void GeneratePythonScript3D();
+
 
 };
 
